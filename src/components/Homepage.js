38734d70ec691991f69af38b12313592c0c4176a
nahathkaank <104026376+nahathkaank@users.noreply.github.com>
2023-04-24 11:56:47 -0700

    text area resizable

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -53,60 +53,54 @@ export const Homepage = () => {
 
       }}>
 
-      <ul>
-        <Box sx={{ p: 1, backgroundColor: 'primary.main' }}>
-          <Typography variant="h2" component="h1" sx={{ fontWeight: 'light', color: 'white' }}>Welcome to DigEdit</Typography>
-          
-          <Typography variant="body" component="hgroup" sx={{ fontWeight: 'light', color: 'white' }}>A visualization application for editing and viewing discussion graphs</Typography>
-        </Box>
-        <br></br><br></br>
-        <Typography variant="h4" component="h2" sx={{ fontWeight: 'light' }}>Please enter the title for the y-axis</Typography>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          placeholder='Food'
-          onChange={handleCli}
 
-        />
-        <br></br>
-        <br></br>
-        <Typography variant="h4" component="h3" sx={{ fontWeight: 'light' }}>Please enter in the format: (name,data) separated by ";"</Typography>
-        <form>
-          <TextareaAutosize
-            aria-label="text area data entry"
-            placeholder="(Toast, 23);(Meat,78);(Salad, 12);(Biscuit, 34);(Cake, 45);(Cookie, 69)" style={{
-              maxHeight: '500px',
-              minHeight: '250px',
-              minWidth: '1000px',
-              resize: 'none',
-              padding: '9px',
-              boxSizing: 'border-box',
-              fontSize: '15px'
-            }} onChange={handleChange} />
+      <Box sx={{ p: 1, backgroundColor: 'primary.main' }}>
+        <Typography variant="h2" component="h1" sx={{ fontWeight: 'light', color: 'white' }}>Welcome to DigEdit</Typography>
 
+        <Typography variant="body" component="hgroup" sx={{ fontWeight: 'light', color: 'white' }}>A visualization application for editing and viewing discussion graphs</Typography>
+      </Box>
+      <br></br>
+      <br></br>
+      <Typography variant="h4" component="h2" sx={{ fontWeight: 'light' }}>Please enter the title for the y-axis</Typography>
+      <input
+        type="text"
+        id="title"
+        name="title"
+        placeholder='Food'
+        onChange={handleCli}
 
+      />
+      <br></br>
+      <br></br>
+      <Typography variant="h4" component="h3" sx={{ fontWeight: 'light' }}>Please enter in the format: (name,data) separated by ";"</Typography>
+      <form>
+        <TextareaAutosize
+          aria-label="text area data entry"
+          minRows={10}
+          placeholder="(Toast, 23);(Meat,78);(Salad, 12);(Biscuit, 34);(Cake, 45);(Cookie, 69)"
+          style={{
+            width: 400
+          }} onChange={handleChange} />
+      </form>
 
-        </form>
+      <Button variant="contained" size="large" onClick={handleClick} >Submit</Button>
+      <br></br>
+      <FormControl sx={{ m: 1, minWidth: 135 }}>
+        <InputLabel id="select-label">Select Graph</InputLabel>
+        <Select
+          labelId="select-label"
+          id="simple-select"
+          value={selectValue}
+          label="Graph Type"
+          onChange={onChange}
+        >
+          <MenuItem value="BarChart">Bar Chart</MenuItem>
+          <MenuItem value="LineChart">Line Chart</MenuItem>
+          <MenuItem value="PieChart">Pie Chart</MenuItem>
+          <MenuItem value="RadarChart">Radar Chart</MenuItem>
+        </Select>
+      </FormControl>
 
-        <Button variant="contained" size="large" onClick={handleClick} >Submit</Button>
-        <br></br>
-        <FormControl sx={{ m: 1, minWidth: 135 }}>
-          <InputLabel id="select-label">Select Graph</InputLabel>
-          <Select
-            labelId="select-label"
-            id="simple-select"
-            value={selectValue}
-            label="Graph Type"
-            onChange={onChange}
-          >
-            <MenuItem value="BarChart">Bar Chart</MenuItem>
-            <MenuItem value="LineChart">Line Chart</MenuItem>
-            <MenuItem value="PieChart">Pie Chart</MenuItem>
-            <MenuItem value="RadarChart">Radar Chart</MenuItem>
-          </Select>
-        </FormControl>
-      </ul>
 
       {(selectValue === "BarChart") && <BarChart name={newMess} />}
       {(selectValue === "LineChart") && <LineChart name={newMess} />}
